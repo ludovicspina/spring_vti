4b78bb2f1637b1b4d263453e2a8daa4fd4ef8da3
JulianCourbez <julian.courbez@gmail.com>
2022-10-25 14:13:25 +0200

    CRUD USER MVC

--- src/main/java/com/julian/spring/repositories/UserRepository.java
+++ src/main/java/com/julian/spring/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package com.julian.spring.repositories;
 import com.julian.spring.models.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface UserRepository extends CrudRepository<User, Integer> {
 
 }
