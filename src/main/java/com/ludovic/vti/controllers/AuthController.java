925c9c3b24383a1adfd9408cac0f529b7a1e6045
Ludovic Spina <74927851+ludovicspina@users.noreply.github.com>
2023-02-09 11:56:34 +0100

    manque le chiffrement

--- src/main/java/com/ludovic/vti/controllers/AuthController.java
+++ src/main/java/com/ludovic/vti/controllers/AuthController.java
@@ -1,16 +1,20 @@
 package com.ludovic.vti.controllers;
 
-import com.ludovic.vti.models.Game;
-import com.ludovic.vti.models.Users;
+import com.ludovic.vti.repositories.UserRepository;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @Controller
 public class AuthController {
@@ -19,6 +23,7 @@ public class AuthController {
     public String showLogin() {
         return "login";
     }
+
 
     @GetMapping("/logout")
     public String logout(HttpServletRequest request, HttpServletResponse response) {
