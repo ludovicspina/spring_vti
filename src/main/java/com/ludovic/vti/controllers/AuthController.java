1385f7f1b820a8d7343c659c88a090adfcb88a40
Ludovic Spina <74927851+ludovicspina@users.noreply.github.com>
2023-02-09 11:25:25 +0100

    working

--- src/main/java/com/ludovic/vti/controllers/AuthController.java
+++ src/main/java/com/ludovic/vti/controllers/AuthController.java
@@ -2,9 +2,15 @@ package com.ludovic.vti.controllers;
 
 import com.ludovic.vti.models.Game;
 import com.ludovic.vti.models.Users;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class AuthController {
@@ -13,4 +19,14 @@ public class AuthController {
     public String showLogin() {
         return "login";
     }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
+    }
+
 }
